49ad3716951e0c3a9b7ed938fd396d605f7d5085
palagpatski <palagpatski@gmail.com>
2015-06-23 12:38:27 +0800

    Implode breadcrumbs.

--- template.php
+++ template.php
@@ -5,3 +5,10 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * Omega VFP theme.
  */
+
+function omega_vfp_breadcrumb($variables) {
+  $sep = ' &raquo; ';
+  if (count($variables['breadcrumb']) > 0) {
+    return implode($sep, $variables['breadcrumb']);
+  }
+}
