dfb4e9ca7b389a71c57a085886c871ec8063c72a
palagpatski <palagpatski@gmail.com>
2015-09-25 11:27:02 +0800

    Override all day label.

--- template.php
+++ template.php
@@ -5,3 +5,10 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * Omega VFP theme.
  */
+
+/**
+ * Override of date_all_day_label.
+ */
+function omega_vfp_date_all_day_label() {
+  return '';
+}
